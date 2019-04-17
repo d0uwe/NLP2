567195ecf9c4dfff84c24cd42b3b2b618c71425d
Tessa Wagenaar <tessawagenaar@quicknet.nl>
2019-04-17 12:02:06 +0200

    IBM1 poging 1

--- IBM1.py
+++ IBM1.py
@@ -3,7 +3,7 @@ IBM model 1
 Tim Smit, Tessa Wagenaar and Douwe van der Wal
 '''
 
-from collections import defaultdict
+from collections import defaultdict, Counter
 from itertools import product
 
 from aer import read_naacl_alignments, AERSufficientStatistics
@@ -15,29 +15,80 @@ def read_data(filename):
 	return e
 
 class IBM1: 
-	def __init__(self):
+	def __init__(self, e_lines, f_lines):
+		self.t = {}
 		self._vocab = {}
 		self._cpd = {}
+		self._alignment_cpd = {}
+		self.e = e_lines
+		self.f = f_lines
+		self._init_t()
 
-	def create_vocab(self, e_lines, f_lines):
-		vocab = defaultdict(lambda: defaultdict(int))
-		for e_sen, f_sen in zip(e_lines, f_lines):
-			e_sen = e_sen.strip().split()
-			f_sen = f_sen.strip().split()
+	def _init_t(self):
+		en_words = set()
+		for e_sen in self.e:
+			for e in e_sen.split():
+				en_words.add(e) 
+
+		fr_words = set()
+		for f_sen in self.f:
+			for f in f_sen.split():
+				fr_words.add(f) 
+
+		self.en_words, self.fr_words = en_words, fr_words
+		print(1/len(en_words))
+
+		self.t = defaultdict(lambda: defaultdict(lambda: 1/len(en_words)))
+
+	# def create_vocab(self):
+	# 	e_lines, f_lines = self.e, self.f
+
+	# 	vocab = defaultdict(lambda: defaultdict(int))
+	# 	alignment = defaultdict(lambda: defaultdict(float))
+
+	# 	for i, (e_sen, f_sen) in enumerate(zip(e_lines, f_lines)):
+	# 		e_sen = e_sen.strip().split()
+	# 		f_sen = f_sen.strip().split()
+	# 		ef = product(range(len(e_sen)), range(len(f_sen)))
+	# 		for idx_e, idx_f in ef:
+	# 			alignments[idx_e][idx_f] 
+	# 			vocab[e][f] += self.t[e][f]
+
+	# 	self._vocab = vocab
+
+	# 	cpd = defaultdict(lambda: defaultdict(float))
+	# 	for e, f_dict in vocab.items():
+	# 		total = sum(f_dict.values())
+	# 		for f, count in f_dict.items():
+	# 			cpd[e][f] = f/total
+
+	# 	self._cpd = cpd
+
+
+	# def create_alignments():
+
+
+	def EM(self):
+		counts = defaultdict(lambda: defaultdict(float))
+		total = defaultdict(float)
+		s_total = defaultdict(float)
+
+		for i, (e_sen, f_sen) in enumerate(zip(self.e, self.f)):
 			ef = product(e_sen, f_sen)
-			for e,f in ef:
-				vocab[e][f] += 1
+			for e, f in ef:
+				s_total[e] += self.t[e][f]
 
-		self._vocab = vocab
+			for e, f in ef:
+				count[e][f] += self.t[e][f] / s_total[e]
+				total[f] += self.t[e][f] / s_total[e]
 
-		cpd = defaultdict(lambda: defaultdict(float))
-		for e, f_dict in vocab.items():
-			total = sum(f_dict.values())
-			for f, count in f_dict.items():
-				cpd[e][f] = f/total
+		for e,f in zip(list(self.en_words), list(self.fr_words)):
+			self.t[e][f] = counts[e][f] / total[f]
 
-		self._cpd = cpd
 
+
+
+		
 
 
 
@@ -45,4 +96,7 @@ class IBM1:
 
 e = read_data("data/training/hansards.36.2.e")
 f = read_data("data/training/hansards.36.2.f")
-print(e[:10])
+ibm1 = IBM1(e, f)
+ibm1.EM()
+print(imb1.t['limits'])
+# print(e[:10])
