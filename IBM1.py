a7b07c4ff9c52282d58fc72c4060b30c65fcbd76
Tessa Wagenaar <tessawagenaar@quicknet.nl>
2019-04-24 15:53:00 +0200

    added aer

--- IBM1.py
+++ IBM1.py
@@ -5,115 +5,139 @@ Tim Smit, Tessa Wagenaar and Douwe van der Wal
 
 from collections import defaultdict, Counter
 from itertools import product
+from random import random
+
 import matplotlib.pyplot as plt
-# import pickle
+import numpy as np
 import dill
 
 from aer import read_naacl_alignments, AERSufficientStatistics
 
 def read_data(filename):
-	with open(filename, 'r') as f:
-		e = f.readlines()
+    with open(filename, 'r') as f:
+        e = f.readlines()
 
-	return e
+    return e
 
 class IBM1: 
-	def __init__(self, e_lines, f_lines):
-		self.t = {}
-		self._vocab = {}
-		self._cpd = {}
-		self._alignment_cpd = {}
-		self.e = e_lines
-		self.f = f_lines
-		self._init_t()
+    def __init__(self, e_lines, f_lines):
+        self.t = {}
+        self._vocab = {}
+        self._cpd = {}
+        self._alignment_cpd = {}
+        self.e = e_lines
+        self.f = f_lines
+        self._init_t()
 
-	def _init_t(self):
+    def _init_t(self):
 
-		en_words = set()
-		for e_sen in self.e:
-			for e in e_sen.split():
-				en_words.add(e) 
+        en_words = set()
+        for e_sen in self.e:
+            for e in e_sen.split():
+                en_words.add(e) 
 
-		fr_words = set()
-		for f_sen in self.f:
-			for f in f_sen.split():
-				fr_words.add(f) 
+        fr_words = set()
+        for f_sen in self.f:
+            for f in f_sen.split():
+                fr_words.add(f) 
 
-		self.en_words, self.fr_words = en_words, fr_words
-		self.t = defaultdict(lambda: defaultdict(lambda: 1/len(en_words)))
+        self.en_words, self.fr_words = en_words, fr_words
+        self.t = defaultdict(lambda: defaultdict(lambda: 1/len(en_words)))
 
-	def logprob(self):
-		logprob = 0
-		for e,f in zip(self.e, self.f):
-			e = ["NULL"] + e.split()
-			f = f.split()
-			m = len(f)
-			l = len(e)
-			pa = 1 / (1 + l)
-			log_pfe = sum([np.log(pa * sum([self.t[e[a]][f[j]] for a in range(l)])) for j in range(m)])
-			logprob += log_pfe
-		return logprob
+    def logprob_sentence(self, e, f):
+        e = ["NULL"] + e.split()
+        f = f.split()
+        m = len(f)
+        l = len(e)
+        pa = 1 / (1 + l)
+        log_pfe = sum([np.log(pa * sum([self.t[e[a]][f[j]] for a in range(l)])) for j in range(m)])
+        return log_pfe
 
-	def EM(self, iterations):
-		logprobs = []
-		for iter in range(iterations):
-			print("Currently running iteration: {0}".format(iter))
-			counts = defaultdict(lambda: defaultdict(float))
-			total = defaultdict(float)
-			s_total = defaultdict(float)
 
-			print("E-step")
-			for e_sen, f_sen in zip(self.e, self.f):
-				e_sen = ["NULL"] + e_sen.split()
-				f_sen = f_sen.split()
+    def logprob(self):
+        logprob = 0
+        for e,f in zip(self.e, self.f):
+            log_pfe = self.logprob_sentence(e,f)
+            logprob += log_pfe
+        return logprob
 
-				ef = product(e_sen, f_sen)
-				for e, f in ef:
-					s_total[e] += self.t[e][f]
-				
-				ef = product(e_sen, f_sen)
-				for e, f in ef:
-					counts[e][f] += self.t[e][f] / s_total[e]
-					total[f] += self.t[e][f] / s_total[e]
+    def evaluate_aer(self):
+        path = 'data/validation/dev.wa.nonullalign'
+        gold_sets = read_naacl_alignments(path)
 
-			
-			print("M-step")
-			for e_sen, f_sen in zip(self.e, self.f):
-				e_sen = ["NULL"] + e_sen.split()
-				f_sen = f_sen.split()
-				ef = product(e_sen, f_sen)
+        predictions = []
+            for s, p in gold_sets:
+                links = set()
+                for link in s:
+                    if random() < 0.5:
+                        links.add(link)
+                predictions.append(links)
 
-				for e, f in ef:
-					self.t[e][f] = counts[e][f] / total[f]
+        metric = AERSufficientStatistics()
 
-			logprobs.append(self.logprob())
+        for gold, pred in zip(gold_sets, predictions):
+            metric.update(sure=gold[0], probable=gold[1], predicted=pred)
 
-		return logprobs
-
-			# for e in self.en_words:
-			# 	for f in self.fr_words:
-			# 		if total[f] == 0:
-			# 			self.t[e][f] = 0.0
-			# 		else:
-			# 			self.t[e][f] = counts[e][f] / total[f]
-
-	def viterbi(self, e_sen, f_sen):
-		'''
-		Input:
-			e_sen: 	an english sentence
-			f_sen: 	a french sentence
-
-		Output:
-			a: 		an optimal alignment
-		'''
-		x=1
+        return metric.aer()
 
 
 
+    def EM(self, iterations):
+        logprobs = []
+        aers = []
+
+        for iter in range(iterations):
+            print("Currently running iteration: {0}".format(iter))
+            counts = defaultdict(lambda: defaultdict(float))
+            total = defaultdict(float)
+            s_total = defaultdict(float)
+
+            print("E-step")
+            for e_sen, f_sen in zip(self.e, self.f):
+                e_sen = ["NULL"] + e_sen.split()
+                f_sen = f_sen.split()
+
+                ef = product(e_sen, f_sen)
+                for e, f in ef:
+                    s_total[e] += self.t[e][f]
+                
+                ef = product(e_sen, f_sen)
+                for e, f in ef:
+                    counts[e][f] += self.t[e][f] / s_total[e]
+                    total[f] += self.t[e][f] / s_total[e]
+
+            
+            print("M-step")
+            for e_sen, f_sen in zip(self.e, self.f):
+                e_sen = ["NULL"] + e_sen.split()
+                f_sen = f_sen.split()
+                ef = product(e_sen, f_sen)
+
+                for e, f in ef:
+                    self.t[e][f] = counts[e][f] / total[f]
+
+            aers.append(self.evaluate_aer())
+            logprobs.append(self.logprob())
+
+        return logprobs, aers
+
+    def viterbi(self, e_sen, f_sen):
+        '''
+        Input:
+            e_sen:  an english sentence
+            f_sen:  a french sentence
+
+        Output:
+            a:      an optimal alignment
+        '''
+        x=1
 
 
 
-		
+
+
+
+        
 
 
 # Change to true if model should be loaded from pickle
@@ -126,15 +150,18 @@ ef = list(set(zip(e,f)))
 e, f = zip(*ef)
 
 if load_model:
-	ibm1 = dill.load(open("ibm1.p", 'r'))
-	logprobs = dill.load(open("logprobs_ibm1.p", 'r'))
+    ibm1 = dill.load(open("ibm1.p", 'rb'))
+    logprobs = dill.load(open("logprobs_ibm1.p", 'rb'))
+    aers = dill.load(open("aers_ibm1.p", 'rb'))
 else:
-	ibm1 = IBM1(e, f)
-	logprobs = ibm1.EM(1)
-	dill.dump(ibm1, open("ibm1.p", 'w'))
-	dill.dump(logprobs, open("logprobs_ibm1.p", 'w'))
+    ibm1 = IBM1(e, f)
+    logprobs, aers = ibm1.EM(1)
+    dill.dump(ibm1, open("ibm1.p", 'wb'))
+    dill.dump(logprobs, open("logprobs_ibm1.p", 'wb'))
+    dill.dump(aers, open("aers_ibm1.p", 'wb'))
 
 
 plt.plot(logprobs)
+plt.plot(aers)
 plt.show()
 
