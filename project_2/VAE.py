a63c705915657ef6e370e60ce516828d885e589d
Tessa Wagenaar <tessawagenaar@quicknet.nl>
2019-05-25 12:07:26 +0200

    Weird bug

--- project_2/VAE.py
+++ project_2/VAE.py
@@ -11,6 +11,7 @@ import torch.nn as nn
 import torch
 from torch.distributions.normal import Normal
 from load_data import LoadData
+from pdb import set_trace
 
 
 # device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -56,6 +57,7 @@ class Decoder(nn.Module):
         self.embedding = nn.Embedding(vocab_len, vocab_dim)
         self.lstm = nn.LSTM(vocab_dim, hidden_dim, num_layers, batch_first=True)
         self.linear_out = nn.Linear(hidden_dim, vocab_len)
+        self.softmax = nn.Softmax(dim=2)
         self.vocab_dim = vocab_dim
         self.hidden_dim = hidden_dim
 
@@ -72,29 +74,47 @@ class Decoder(nn.Module):
         sen = self.linear_out(out)
         return sen
 
+    def sample(self, z, BOS_id, sample_length):
+        h = self.tanh(self.linear_h(z)).reshape(1, -1, self.hidden_dim)
+        c = torch.zeros_like(h)
+        x = torch.tensor([[BOS_id]])
+        sample = [BOS_id]
+        for i in range(sample_length):
+            e = self.embedding(x)
+            out, (h,c) = self.lstm(e, (h,c))
+            x = self.softmax(out).multinomial(1).reshape(1,1)
+            sample.append(x.item())
+
+        return sample
+
+
 
 
 class SentenceVAE(nn.Module):
     def __init__(self, vocab_len, vocab_dim, z_dim, hidden_dim, padding_idx, num_layers=1):
         super(SentenceVAE, self).__init__()
         self.z_dim = z_dim
+        self.vocab_size = vocab_len
         self.encoder = Encoder(vocab_len, vocab_dim, z_dim, hidden_dim, padding_idx, num_layers=num_layers)
         self.decoder = Decoder(vocab_len, vocab_dim, z_dim, hidden_dim, padding_idx, num_layers=num_layers)
-        criterion = nn.CrossEntropyLoss()
+        self.softmax = nn.Softmax()
 
-    def forward(self, x):
+    def forward(self, x, y):
         mu, sigma = self.encoder(x)
         epsilon = torch.randn(self.z_dim).to(device)
         z = mu + sigma * epsilon
         sentence = self.decoder(z, x)
 
+        y_hat = sentence.view(-1, self.vocab_size)
+        y = y.view(-1)
+
         kl_loss = -0.5 * (1 + sigma.log() - mu.pow(2) - sigma).sum()
-        recon_loss = nn.functional.cross_entropy(immean, x, reduction='sum')
+        recon_loss = nn.functional.cross_entropy(y_hat, y, reduction='sum')
 
         return (kl_loss + recon_loss) / x.shape[0]
 
         
-    def sample(self, n_samples):
+    def sample(self, n_samples, BOS_id, sample_length):
         """
         Sample n_samples from the model. Return both the sampled images
         (from bernoulli) and the means for these bernoullis (as these are
@@ -103,15 +123,13 @@ class SentenceVAE(nn.Module):
         sampled_sens = []
 
         for _ in range(n_samples):
-            z = torch.normal(torch.zeros(self.z_dim), torch.ones(self.z_dim)).to(device)
-            sentence = self.decoder(z)
-            # CREATE SENTENCE
+            z = torch.randn(self.z_dim).to(device)
+            sentence = self.decoder.sample(z, BOS_id, sample_length)
             sampled_sens.append(sentence)
  
         # rt = np.sqrt(n_samples)
         # sampled_ims = torch.stack(sampled_ims, 0).reshape(rt, rt, 1, 28, 28)
-        return sampled_ims, im_means
-
+        return sampled_sens
 
 
 
@@ -140,14 +158,23 @@ def main():
 
         # Create x and y
         sen = dataset.next_batch(config.batch_size)
-        x = torch.tensor(sen).to(device)
-        # y = torch.tensor(sen[:,1:]).to(device)
+        x = torch.tensor(sen[:,:-1]).to(device)
+        y = torch.tensor(sen[:,1:]).to(device)
 
-        loss = model(x)
+        loss = model(x, y)
         loss.backward()
         optimizer.step()
 
         results["elbo"].append(loss.item())
+
+        print(f"[Step {step}] train elbo: {loss}")
+
+        # Sample from model
+        if step % config.sample_every == 0:
+            sentences = model.sample(5, dataset.get_id("BOS"), config.sample_length)
+            for s in sentences:
+                print(dataset.convert_to_string(s))
+
 
     torch.save(model, open("SentenceVAE.pt", 'wb'))
     plt.figure(figsize=(12, 6))
@@ -163,7 +190,7 @@ def main():
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--epochs', default=40, type=int,
+    parser.add_argument('--epochs', default=2000, type=int,
                         help='max number of epochs')
     parser.add_argument('--z_dim', default=20, type=int,
                         help='dimensionality of latent space')
@@ -173,6 +200,10 @@ if __name__ == "__main__":
                         help="hidden dimension LSTMs")
     parser.add_argument('--batch_size', default=64, type=int, 
                         help="batch size")
+    parser.add_argument('--sample_every', default=100, type=int, 
+                        help="sample interval")
+    parser.add_argument('--sample_length', default=100, type=int, 
+                        help="sample size")
 
     config = parser.parse_args()
 
